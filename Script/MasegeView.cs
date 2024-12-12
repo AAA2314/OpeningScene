0e1958a084ce93dea646c86398da9bbde3e61ad1
AAA2314 <b1023105@fun.ac.jp>
2024-12-12 20:42:54 +0900

    Opening

---
+++ Script/MasegeView.cs
@@ -0,0 +1,45 @@
+using UnityEngine;
+using Cysharp.Threading.Tasks; //「UniTask」を使用するため。
+
+
+public class MasegeView : ViewBase
+{
+    void Start()
+    {
+    }
+
+    // -------------------------------------------------------
+    // ビューオープン時コール.
+    // -------------------------------------------------------
+    public override async void OnViewOpened()
+    {
+        base.OnViewOpened();
+
+        await ChangeViewWaitForSeconds();
+    }
+
+    // -------------------------------------------------------
+    // ビュークローズ時コール.
+    // -------------------------------------------------------
+    public override void OnViewClosed()
+    {
+        base.OnViewClosed();
+    }
+
+    // ------------------------------------------------
+    /// 2秒待ってタイトルViewに移動.
+    // ------------------------------------------------
+    async UniTask ChangeViewWaitForSeconds(float waitTime = 2f)
+    {
+        try
+        {
+            await UniTask.Delay((int)(waitTime * 1000f), false, PlayerLoopTiming.Update, this.GetCancellationTokenOnDestroy());
+            await Scene.ChangeView(2);
+            //(非同期処理).Forget() : 非同期処理を待機せずに実行.
+        }
+        catch (System.OperationCanceledException e)
+        {
+            Debug.Log("ChangeViewWaitForSecondsがキャンセルされました。" + e);
+        }
+    }
+}
---
+++ Script/OpnenigScene.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+public class OpnenigScene : SceneBase
+{
+    protected override void Start()
+    {
+        base.Start();
+    }
+}
